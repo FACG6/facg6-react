1e7e6d2bf2fa63f2ada63218c62acaf82a101c20
AymanAlQoqa <aalqouqa@gmail.com>
2019-04-05 18:08:26 +0300

    create footer relates#3

--- src/components/layout/footer/index.jsx
+++ src/components/layout/footer/index.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import './style.css';
+import React from "react";
+import "./style.css";
 
 const Footer = () => {
-    return (
-        <div className='footer'>
-           <span>CopyRights &copy; 2019</span>
-        </div>
-    );
-}
+  return (
+    <div className="footer">
+      <span>CopyRights &copy; 2019</span>
+    </div>
+  );
+};
 
 export default Footer;
